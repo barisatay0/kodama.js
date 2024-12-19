607b70e9ef5d9a29ded4884dbaf0ad8fa415b933
Barış Atay <barisatay0@hotmail.com>
2024-12-19 14:42:44 +0300

    Create api automation

---
+++ console/create/CreateApi.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+function CreateApi(name) {
+
+}
+
+module.exports = CreateApi;
--- console/create/index.js
+++ console/create/index.js
@@ -2,10 +2,12 @@ const CreateController = require('./CreateController');
 const CreateModel = require('./CreateModel');
 const CreateService = require('./CreateService');
 const CreateCommand = require('./CreateCommand');
+const CreateApi = require('./CreateApi');
 
 module.exports = {
     CreateController,
     CreateModel,
     CreateService,
     CreateCommand,
+    CreateApi,
 };
