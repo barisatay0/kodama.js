2f7e74e58183c394915c987899e5f035de4c552d
Barış Atay <barisatay0@hotmail.com>
2024-12-19 17:14:19 +0300

    *

--- console/create/Create.js
+++ console/create/Create.js
@@ -4,7 +4,6 @@ const path = require('path');
 function Create(name, type) {
     const targetPath = path.join(__dirname, `../../src/${type}s/${name}.js`);
     if (fs.existsSync(targetPath)) return console.error(`${type.charAt(0).toUpperCase() + type.slice(1)} ${name} exists!`);
-
     const content = fs.readFileSync(path.join(__dirname, `../templates/${type.charAt(0).toUpperCase() + type.slice(1)}.js`), 'utf-8')
         .replace(/variable/g, name)
         .replace(/variant/g, name.toLowerCase());
