ed19b56716c0c1739845c05ad7dc5ff75ee0a063
Barış Atay <barisatay0@hotmail.com>
2024-12-19 16:55:23 +0300

    *

--- console/commands.js
+++ console/commands.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-const {Create} = require('./create');
+const { Create } = require('./create');
 const args = process.argv.slice(2);
 const command = args[0];
 const type = args[1];
 const name = args[2];
 
-if (command === 'create') {Create(name,type);}
+const CreateTypes = ['controller', 'service', 'model'];
+
+if (command === 'create' && CreateTypes.includes(type)) {Create(name, type);}
