f0aa9db731320918b563f25577170bbd431e2618
Barış Atay <barisatay0@hotmail.com>
2024-12-19 17:12:42 +0300

    *

--- console/commands.js
+++ console/commands.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
-const { Create } = require('./create');
+const {Create} = require('./create');
 const args = process.argv.slice(2);
 const command = args[0];
 const type = args[1];
 const name = args[2];
 
-const CreateTypes = ['controller', 'service', 'model'];
+const valid_command = ['create'];
+const valid_type = ['controller', 'service', 'model'];
 
-if (command === 'create' && CreateTypes.includes(type)) {Create(name, type);}
-else {
-    console.log( 'There is no command like this.');
-    console.log( 'Our command syntax is looking like "matic command type name" ');
-    console.log( 'Example "matic create controller Controller_name" ');
+if (valid_command.includes(command)) {
+    if (valid_type.includes(type)) {Create(name, type);}
+    else {
+        console.log('There is no command like this.');
+        console.log('Our command syntax is looking like "matic command type name" ');
+        console.log('Example "matic create controller Controller_name" ');
+    }
 }
