7a173b72b5acc3b1e421721452582c1122ed366e
Barış Atay <barisatay0@hotmail.com>
2024-12-19 17:04:50 +0300

    *

--- console/commands.js
+++ console/commands.js
@@ -8,3 +8,8 @@ const name = args[2];
 const CreateTypes = ['controller', 'service', 'model'];
 
 if (command === 'create' && CreateTypes.includes(type)) {Create(name, type);}
+else {
+    console.log( 'There is no command like this.');
+    console.log( 'Our command syntax is looking like "matic command type name" ');
+    console.log( 'Example "matic create controller Controller_name" ');
+}
