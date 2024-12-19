bf62463bcb83be507c97c88821182063987680ae
Barış Atay <barisatay0@hotmail.com>
2024-12-19 15:27:26 +0300

    arg type is added

--- console/commands.js
+++ console/commands.js
@@ -2,11 +2,11 @@
 const {CreateController, CreateModel, CreateService, CreateCommand} = require('./create');
 const args = process.argv.slice(2);
 const command = args[0];
-const subcommand = args[1];
+const type = args[1];
 const name = args[2];
 
-if (command === 'create' && subcommand === 'controller') {CreateController(name);}
-if (command === 'create' && subcommand === 'model') {CreateModel(name);}
-if (command === 'create' && subcommand === 'service') {CreateService(name);}
-if (command === 'create' && subcommand === 'command'){CreateCommand(name);}
-if (command === 'create' && subcommand === 'api') {CreateModel(name);CreateService(name);CreateController(name);}
+if (command === 'create' && type === 'controller') {CreateController(name,type);}
+if (command === 'create' && type === 'model') {CreateModel(name,type);}
+if (command === 'create' && type === 'service') {CreateService(name,type);}
+if (command === 'create' && type === 'command'){CreateCommand(name,type);}
+if (command === 'create' && type === 'api') {CreateModel(name,type);CreateService(name,type);CreateController(name,type);}
