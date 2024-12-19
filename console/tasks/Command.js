b775b479aae0c6f2707f237ae995007c939c8383
Barış Atay <barisatay0@hotmail.com>
2024-12-19 10:57:25 +0300

    Empty command

--- console/tasks/Command.js
+++ console/tasks/Command.js
@@ -1,28 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+function variableCommand(name){
 
-// This example is a create function
-
-function variable(name){
-    const variable = path.join(__dirname, `../../src/models/variable.js`);
-    const Checkvariable = path.join(__dirname, `../../src/models/${name}.js`);
-
-    if (fs.existsSync(Checkvariable)) {
-        console.error(`Model ${name} already exists at ${Checkvariable}`);
-        process.exit(1);
-    }
-    if (!fs.existsSync(variable)) {
-        console.error(`Example file not found: ${variable}`);
-        process.exit(1);
-    }
-
-    const template = fs.readFileSync(variable, 'utf-8');
-    const content = template
-        .replace(/variable/g, name)
-        .replace(/variant/g, name.toLowerCase());
-
-    fs.writeFileSync(Checkvariable, content, 'utf-8');
-    console.log(`Success: variable ${name} created successfully at ${Checkvariable}`);
 }
 
-module.exports = variable;
+module.exports = variableCommand;
