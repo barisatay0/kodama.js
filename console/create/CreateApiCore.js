eb60dc7c39e745561947daab968fa30ae9329dfa
Barış Atay <barisatay0@hotmail.com>
2024-12-24 17:03:53 +0300

    *

--- console/create/CreateApiCore.js
+++ console/create/CreateApiCore.js
@@ -2,11 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 function CreateApiCore(name, type) {
+
     const targetPath = path.join(__dirname, `../../src/${type}s/${name}${type.charAt(0).toUpperCase() + type.slice(1)}.js`);
 
-    if (fs.existsSync(targetPath)) {
-        return console.error(`${type} ${name} exists!`);
-    }
+    if (fs.existsSync(targetPath)) {return console.error(`${type} ${name} exists!`);}
 
     const content = fs.readFileSync(
         path.join(__dirname, `../templates/${type}.js`),
@@ -21,26 +20,20 @@ function CreateApiCore(name, type) {
     if (type === "model") {
         const indexPath = path.join(__dirname, '../../src/models/index.js');
 
-        if (!fs.existsSync(indexPath)) {
-            fs.writeFileSync(indexPath, `module.exports = {};\n`, 'utf-8');
-        }
+        if (!fs.existsSync(indexPath)) {fs.writeFileSync(indexPath, `module.exports = {};\n`, 'utf-8');}
 
         const modelImport = `const { ${name} } = require('./${name}Model');\n`;
         const modelExport = `${name}`;
 
         let indexContent = fs.readFileSync(indexPath, 'utf-8');
 
-        if (!indexContent.includes(modelImport)) {
-            indexContent = modelImport + indexContent;
-        }
+        if (!indexContent.includes(modelImport)) {indexContent = modelImport + indexContent;}
 
         const exportsRegex = /module\.exports = \{([\s\S]*?)};/;
         indexContent = indexContent.replace(exportsRegex, (match, p1) => {
             const existingExports = p1.split(',').map(e => e.trim()).filter(Boolean);
 
-            if (!existingExports.includes(modelExport)) {
-                existingExports.push(modelExport);
-            }
+            if (!existingExports.includes(modelExport)) {existingExports.push(modelExport);}
 
             return `module.exports = {\n    ${existingExports.join(',\n    ')}\n};`;
         });
