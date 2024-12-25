745bec6cdd1e6e07258cb16864c8e5988d3819ce
Barış Atay <barisatay0@hotmail.com>
2024-12-25 16:01:40 +0300

    New api command update

--- console/create/CreateApi.js
+++ console/create/CreateApi.js
@@ -1,22 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const CreateIndex = require("./CreateIndex");
-
-function CreateFile(type, name) {
-    const filePath = path.join(__dirname, `../../src/${type}s/${name}${type.charAt(0).toUpperCase() + type.slice(1)}.js`);
-    if (fs.existsSync(filePath)) {
-        console.error(`${type.charAt(0).toUpperCase() + type.slice(1)} ${name} already exists!`);
-        return false;
-    }
-
-    const content = fs.readFileSync(path.join(__dirname, `../templates/${type}.js`), 'utf-8')
-        .replace(/variable/g, name)
-        .replace(/variant/g, name.toLowerCase());
-    fs.writeFileSync(filePath, content, 'utf-8');
-    console.log(`Success: ${type.charAt(0).toUpperCase() + type.slice(1)} ${name} created`);
-    if(type === 'model'){CreateIndex(name, type);}
-    return true;
-}
+const CreateFile = require("./CreateFile");
 
 function CreateApi(name) {
     const types = ['controller', 'model', 'service'];
