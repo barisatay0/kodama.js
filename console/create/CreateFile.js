1656e5cb326df1fd9f58cadbd5d74a165f713c1d
Barış Atay <barisatay0@hotmail.com>
2024-12-26 12:02:25 +0300

    *

--- console/create/CreateFile.js
+++ console/create/CreateFile.js
@@ -4,15 +4,21 @@ const CreateIndex = require("./CreateIndex");
 const CreateRoute = require("./CreateRoute");
 
 function CreateFile(type, name) {
-    const filePath = path.join(__dirname, `../../src/${type}s/${name}${type.charAt(0).toUpperCase() + type.slice(1)}.js`);
+    const dirPath = path.join(__dirname, `../../src/${type}s`);
+    const filePath = path.join(dirPath, `${name}${type.charAt(0).toUpperCase() + type.slice(1)}.js`);
+
     if (fs.existsSync(filePath)) {console.error(`${type.charAt(0).toUpperCase() + type.slice(1)} ${name} already exists!`);return false;}
+    if (!fs.existsSync(dirPath)) {fs.mkdirSync(dirPath, { recursive: true });console.log(`Directory ${dirPath} created.`);}
 
     const content = fs.readFileSync(path.join(__dirname, `../templates/${type}.js`), 'utf-8')
         .replace(/variable/g, name)
         .replace(/variant/g, name.toLowerCase());
+
     fs.writeFileSync(filePath, content, 'utf-8');
     console.log(`Success: ${type.charAt(0).toUpperCase() + type.slice(1)} ${name} created`);
-    if(type === 'model'){CreateIndex(name, type);CreateRoute(name, type);}
+
+    if (type === 'model') {CreateIndex(name, type);CreateRoute(name, type);}
+
     return true;
 }
 
